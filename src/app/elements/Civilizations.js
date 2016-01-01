2102702e65ffc0c40312724cdb8f4bf768a83bf4
sebdez <sebastien.dez@gmail.com>
2016-01-01 18:55:45 +0100

    translation for civ female

--- src/app/elements/Civilizations.js
+++ src/app/elements/Civilizations.js
@@ -198,5 +198,173 @@ module.exports = {
             name: 'GENERATOR.CIVILIZATIONS.ELF.FEMALE',
             logo: 'woman-elf-face',
             tag: 'ELF'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.DUERGAR.FEMALE',
+            logo: 'dwarf-face',
+            tag: 'DUERGAR'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.DROW.FEMALE',
+            logo: 'woman-elf-face',
+            tag: 'DROW'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.WOODELVES.FEMALE',
+            logo: 'woman-elf-face',
+            tag: 'WOODELVES'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.KHAJIIT.FEMALE',
+            logo: 'khajiit',
+            tag: 'KHAJIIT'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.ORC.FEMALE',
+            logo: 'orc-head',
+            tag: 'ORC'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.GOBLIN.FEMALE',
+            logo: 'orc-head',
+            tag: 'GOBLIN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.URUKHAI.FEMALE',
+            logo: 'orc-head',
+            tag: 'URUKHAI'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.LIZARDMEN.FEMALE',
+            logo: 'rex',
+            tag: 'LIZARDMEN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.GNOME.FEMALE',
+            logo: 'pie-slice',
+            tag: 'GNOME'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.HOBBIT.FEMALE',
+            logo: 'pie-slice',
+            tag: 'HOBBIT'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.HALFLING.FEMALE',
+            logo: 'pie-slice',
+            tag: 'HALFLING'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.TROLL.FEMALE',
+            logo: 'condylura-skull',
+            tag: 'TROLL'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.OGRE.FEMALE',
+            logo: 'condylura-skull',
+            tag: 'OGRE'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.GIANT.FEMALE',
+            logo: 'condylura-skull',
+            tag: 'GIANT'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.GHOST.FEMALE',
+            logo: 'ghost',
+            tag: 'GHOST'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.DAEMON.FEMALE',
+            logo: 'daemon-skull',
+            tag: 'DEMON'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.ANGEL.FEMALE',
+            logo: 'angel-outfit',
+            tag: 'ANGEL'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.FAIRY.FEMALE',
+            logo: 'fairy',
+            tag: 'FAIRY'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.ENT.FEMALE',
+            logo: 'evil-tree',
+            tag: 'ENT'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.SYLVARI.FEMALE',
+            logo: 'falling-leaf',
+            tag: 'SYLVARI'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.BEASTMEN.FEMALE',
+            logo: 'triple-claws',
+            tag: 'BEASTMEN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.LYCAN.FEMALE',
+            logo: 'wolf-head',
+            tag: 'LYCAN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.VAMPIRE.FEMALE',
+            logo: 'pretty-fangs',
+            tag: 'VAMPIRE'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.GARGOYLE.FEMALE',
+            logo: 'robot-golem',
+            tag: 'GARGOYLE'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.QUNARI.FEMALE',
+            logo: 'horn-internal',
+            tag: 'QUNARI'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.SKAVEN.FEMALE',
+            logo: 'moon-claws',
+            tag: 'SKAVEN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.AMPHIBIAN.FEMALE',
+            logo: 'angler-fish',
+            tag: 'AMPHIBIAN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.MINOTAUR.FEMALE',
+            logo: 'minotaur',
+            tag: 'MINOTAUR'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.CENTAUR.FEMALE',
+            logo: 'horse-head',
+            tag: 'CENTAUR'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.DJINN.FEMALE',
+            logo: 'ifrit',
+            tag: 'DJINN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.DRYAD.FEMALE',
+            logo: 'falling-leaf',
+            tag: 'DRYAD'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.DRAGONBORN.FEMALE',
+            logo: 'dragon-head',
+            tag: 'DRAGONBORN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.FAUN.FEMALE',
+            logo: 'hoof',
+            tag: 'FAUN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.GHOUL.FEMALE',
+            logo: 'half-body-crawling',
+            tag: 'GHOUL'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.MINOBOAR.FEMALE',
+            logo: 'boar-tusks',
+            tag: 'MINOBOAR'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.SIREN.FEMALE',
+            logo: 'harpy',
+            tag: 'SIREN'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.NAGA.FEMALE',
+            logo: 'harpy',
+            tag: 'NAGA'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.MERMAID.FEMALE',
+            logo: 'harpy',
+            tag: 'MERMAID'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.HALFINSECT.FEMALE',
+            logo: 'praying-mantis',
+            tag: 'HALFINSECT'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.GOLEM.FEMALE',
+            logo: 'robot-golem',
+            tag: 'GOLEM'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.GOAULD.FEMALE',
+            logo: 'apple-maggot',
+            tag: 'GOAULD'
+    },{
+            name: 'GENERATOR.CIVILIZATIONS.CYCLOP.FEMALE',
+            logo: 'cyclops',
+            tag: 'CYCLOP'
     }]
 };
