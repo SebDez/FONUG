da801f1f061f091a5993861f13dcc02b8e4a94a2
sebdez <sebastien.dez@gmail.com>
2016-01-24 17:20:32 +0100

    add new corpulences

--- src/app/elements/Corpulences.js
+++ src/app/elements/Corpulences.js
@@ -12,10 +12,10 @@ module.exports = [
         }
     },
     {
-        name:{
+        name: {
             MALE: 'GENERATOR.CORPULENCES.HUNCHBACKED.MALE',
             FEMALE: 'GENERATOR.CORPULENCES.HUNCHBACKED.FEMALE'
-        } 
+        }
     },
     {
         name: {
@@ -135,6 +135,117 @@ module.exports = [
         name: {
             MALE: 'GENERATOR.CORPULENCES.CHARMING.MALE',
             FEMALE: 'GENERATOR.CORPULENCES.CHARMING.FEMALE'
+        }
+    },
+    {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.NORMAL.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.NORMAL.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.SKINNY.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.SKINNY.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.STRAIGHT.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.STRAIGHT.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.AGILE.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.AGILE.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.CONFIDENT.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.CONFIDENT.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.CAPTIVATING.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.CAPTIVATING.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.SMALL.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.SMALL.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.REINFORCED.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.REINFORCED.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.SPORTS.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.SPORTS.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.FELINE.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.FELINE.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.LIGHTWEIGHT.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.LIGHTWEIGHT.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.LIGHTWEIGHTATHLETIC.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.LIGHTWEIGHTATHLETIC.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.ATHLETICMEDIUM .MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.ATHLETICMEDIUM .FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.HEAVYATHLETIC .MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.HEAVYATHLETIC .FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.LARGE.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.LARGE.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.OVERWEIGHTGENTLE.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.OVERWEIGHTGENTLE.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.LIGHTUNDERWEIGHT.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.LIGHTUNDERWEIGHT.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.LIGHTOVERWEIGHT .MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.LIGHTOVERWEIGHT .FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.FLEXIBLE.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.FLEXIBLE.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.IDEAL.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.IDEAL.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.COMMON.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.COMMON.FEMALE'
+        }
+    }, {
+        name: {
+            MALE: 'GENERATOR.CORPULENCES.AERODYNAMIC.MALE',
+            FEMALE: 'GENERATOR.CORPULENCES.AERODYNAMIC.FEMALE'
         }
     }
 ];
